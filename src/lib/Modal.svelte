ea7cbdf388b66a6b4cc1435160a3b72c5ebf167e
Robin Métral <robin.metral@sumup.com>
2021-11-28 14:46:31 +0100

    Remove Storybook example files

---
+++ src/lib/Modal.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  export let label = "Button";
+
+  const dispatch = createEventDispatcher();
+  function onClick(event) {
+    dispatch("click", event);
+  }
+</script>
+
+<button type="button" on:click={onClick}>
+  {label}
+</button>
---
+++ src/stories/Modal.stories.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+  import Modal from "../lib/Modal.svelte";
+</script>
+
+<Meta
+  title="Modal"
+  component={Modal}
+  argTypes={{
+    label: { control: "text" },
+    onClick: { action: "onClick" },
+  }}
+/>
+
+<Template let:args>
+  <Modal {...args} on:click={args.onClick} />
+</Template>
+
+<Story
+  name="Base"
+  args={{
+    label: "Button",
+  }}
+/>
