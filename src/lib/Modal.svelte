053eefc127844eec054d249823ac826e1fd0d619
Robin Métral <robin.metral@sumup.com>
2021-11-29 22:48:29 +0100

    Add base modal

--- src/lib/Modal.svelte
+++ src/lib/Modal.svelte
@@ -1,14 +1,83 @@
-<script>
-  import { createEventDispatcher } from "svelte";
-
-  export let label = "Button";
-
-  const dispatch = createEventDispatcher();
-  function onClick(event) {
-    dispatch("click", event);
-  }
+<script lang="ts">
+  import MicroModal from "micromodal";
+  import { onMount } from "svelte";
+  onMount(() => {
+    MicroModal.init();
+  });
+  export let title: string;
+  export let id: string;
+  export let closeLabel = "Close modal";
+  export let closeIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24"height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"> <line x1="18" y1="6" x2="6" y2="18" /> <line x1="6" y1="6" x2="18" y2="18" /></svg>`;
 </script>
 
-<button type="button" on:click={onClick}>
-  {label}
-</button>
+<div class="mm-modal" {id} aria-hidden="true">
+  <div class="mm-overlay" tabindex="-1" data-micromodal-close>
+    <div
+      class="mm-container"
+      role="dialog"
+      aria-modal="true"
+      aria-labelledby={`${id}-title`}
+      {...$$restProps}
+    >
+      <header class="mm-header">
+        <h2 id={`${id}-title`}>{title}</h2>
+        <button class="mm-close" aria-label={closeLabel} data-micromodal-close>
+          {@html closeIcon}
+        </button>
+      </header>
+      <slot />
+    </div>
+  </div>
+</div>
+
+<style>
+  /* TODO: default animations */
+
+  .mm-modal {
+    display: none;
+  }
+
+  /* Can we somehow avoid the :global() here? */
+  .mm-modal:global(.is-open) {
+    display: block;
+  }
+
+  .mm-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 100;
+  }
+
+  .mm-container {
+    background-color: white;
+    padding: 1.5rem;
+    width: 512px;
+    border-radius: 0.5rem;
+    /* handles high modals */
+    max-height: 100vh;
+    overflow-y: auto;
+  }
+
+  .mm-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .mm-close {
+    cursor: pointer;
+    display: flex;
+    padding: 0.5rem;
+    border-radius: 100%;
+    /* reset */
+    background: none;
+    border: none;
+  }
+</style>
--- src/stories/Modal.stories.svelte
+++ src/stories/Modal.stories.svelte
@@ -7,18 +7,35 @@
   title="Modal"
   component={Modal}
   argTypes={{
-    label: { control: "text" },
-    onClick: { action: "onClick" },
+    id: { control: "text" },
+    title: { control: "text" },
   }}
 />
 
 <Template let:args>
-  <Modal {...args} on:click={args.onClick} />
+  <button data-micromodal-trigger="modal-1">Open modal</button>
+  <Modal {...args}>
+    <p>Are you sure you want to do this thing?</p>
+    <div class="actions">
+      <button on:click={() => console.log("Continued")}>Yes, continue</button>
+      <button data-micromodal-close>No, cancel</button>
+    </div>
+  </Modal>
+  <style>
+    p {
+      margin: 1rem 0;
+    }
+    .actions {
+      display: flex;
+      gap: 1rem;
+    }
+  </style>
 </Template>
 
 <Story
   name="Base"
   args={{
-    label: "Button",
+    id: "modal-1",
+    title: "Wait a minute",
   }}
 />
