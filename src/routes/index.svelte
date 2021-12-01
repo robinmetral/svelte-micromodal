ece1d056708aa3f31a9d4d6f00688befa4fa85f2
Robin Métral <robin.metral@sumup.com>
2021-12-01 20:59:23 +0100

    Write homepage intro

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,22 +9,48 @@
   >
 </svelte:head>
 
-<h1>svelte-micromodal</h1>
-<button data-micromodal-trigger="modal-1">Do the thing</button>
-<Modal title="Wait a minute" id="modal-1" closeLabel="Close modal">
-  <p>Are you sure you want to do this thing?</p>
-  <div class="actions">
-    <button on:click={() => console.log("Continued")}>Yes, continue</button>
-    <button data-micromodal-close>No, cancel</button>
-  </div>
-</Modal>
+<main>
+  <h1>svelte-micromodal</h1>
+  <p>
+    <code>svelte-micromodal</code> is a small
+    <a
+      href="https://github.com/sveltejs/svelte"
+      title="Svelte repository on GitHub">Svelte</a
+    >
+    wrapper around
+    <a
+      href="https://github.com/Ghosh/micromodal"
+      title="Micromodal repository on GitHub">Micromodal</a
+    >, a small JavaScript library for creating accessible modal dialogs.
+  </p>
+  <button data-micromodal-trigger="modal-1">Do the thing</button>
+  <Modal title="Wait a minute" id="modal-1" closeLabel="Close modal">
+    <p class="modal-body">Are you sure you want to do this thing?</p>
+    <div class="modal-actions">
+      <button on:click={() => console.log("Continued")}>Yes, continue</button>
+      <button data-micromodal-close>No, cancel</button>
+    </div>
+  </Modal>
+</main>
 
 <style>
-  p {
-    margin: 1rem 0;
+  main {
+    max-width: 768px;
+    padding: 48px 16px;
+    margin: 0 auto;
   }
-  .actions {
+  h1 {
+    text-align: center;
+    margin-bottom: 24px;
+  }
+  p {
+    margin-bottom: 24px;
+  }
+  .modal-body {
+    margin-top: 16px;
+  }
+  .modal-actions {
     display: flex;
-    gap: 1rem;
+    gap: 16px;
   }
 </style>
